143afc25bcb61a97f3024470f1188e106e9dff2f
Oleksandr Hubanov <alex@0x0h.com>
2020-07-31 00:55:53 +0300

    fix of inverted logic of hid_init and hid_exit return codes

--- hidapiw_native/hidapiw_native.cpp
+++ hidapiw_native/hidapiw_native.cpp
@@ -2,20 +2,41 @@
 
 #include "hidapiw_native.h"
 
+inline hidapiw_native::hidapiw_native()
+{
+	if (hid_init())
+	{
+		throw "failed to init hidapi";
+	}
+}
+
+inline hidapiw_native::~hidapiw_native() {
+	if (m_isDisposed)
+		return;
+
+	// dispose managed data
+	//delete m_managedData; 
+	this->!hidapiw_native(); // call finalizer
+	m_isDisposed = true;
+}
+
 void hidapiw_native::enumerate(System::Collections::Generic::List<hidDeviceInfo^>^% devs, unsigned short vendorID, unsigned short productID)
 {
 	_devs = hid_enumerate(vendorID, productID);
-	System::Collections::Generic::List<hidDeviceInfo^>^ list = gcnew System::Collections::Generic::List<hidDeviceInfo^>();
-	while (_devs->next != nullptr)
+	devs = gcnew System::Collections::Generic::List<hidDeviceInfo^>();
+	struct hid_device_info* cur_dev = _devs;
+	while (cur_dev != nullptr)
 	{
 		auto tmp = gcnew hidDeviceInfo();
-		tmp->interface_number = _devs->interface_number;
+		tmp->interface_number = cur_dev->interface_number;
 		//tmp->manufacturer_string = String(_devs->manufacturer_string);
 		//tmp->path = String(_devs->path);
-		tmp->product_id = _devs->product_id;
+		tmp->product_id = cur_dev->product_id;
 		//tmp->product_string = (String)_devs->product_string;
-		tmp->release_number = _devs->release_number;
+		tmp->release_number = cur_dev->release_number;
+		tmp->vendor_id = cur_dev->vendor_id;
 		//tmp->serial_number = _devs->serial_number;
 		devs->Add(tmp);
+		cur_dev = cur_dev->next;
 	}
 }
--- hidapiw_native/hidapiw_native.h
+++ hidapiw_native/hidapiw_native.h
@@ -10,22 +10,8 @@ private:
 	struct hid_device_info* _devs;
 	bool m_isDisposed; // must be set to false
 public:
-	hidapiw_native()
-	{
-		if (!hid_init())
-		{
-			throw "failed to init hidapi";
-		}
-	}
-	~hidapiw_native() {
-		if (m_isDisposed)
-			return;
-
-		// dispose managed data
-		//delete m_managedData; 
-		this->!hidapiw_native(); // call finalizer
-		m_isDisposed = true;
-	}
+	hidapiw_native();
+	~hidapiw_native();
 
 	// Finalizer
 	!hidapiw_native() {
@@ -34,7 +20,7 @@ public:
 		{
 			hid_free_enumeration(_devs);
 		}
-		if (!hid_exit())
+		if (hid_exit())
 		{
 			throw "failed to exit hidapi";
 		} 
