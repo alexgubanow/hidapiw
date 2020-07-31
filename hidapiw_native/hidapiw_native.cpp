2b7a641884caec136ae5ef700de4310d47906bb2
Oleksandr Hubanov <oleksandr.Hubanov@seraphimas-hls.com>
2020-07-31 15:11:05 +0300

    add most of original api to native wrapper

--- hidapiw_native/hidapiw_native.cpp
+++ hidapiw_native/hidapiw_native.cpp
@@ -2,7 +2,7 @@
 #define NATIVE_CODE
 #include "hidapiw_native.h"
 
-hidapiw_native::hidapiw_native() : _devs(nullptr)
+hidapiw_native::hidapiw_native()
 {
 	if (hid_init())
 	{
@@ -12,10 +12,6 @@ hidapiw_native::hidapiw_native() : _devs(nullptr)
 
 hidapiw_native::~hidapiw_native()
 {
-	if (_devs != nullptr)
-	{
-		hid_free_enumeration(_devs);
-	}
 	if (hid_exit())
 	{
 		throw "failed to exit hidapi";
@@ -24,8 +20,12 @@ hidapiw_native::~hidapiw_native()
 
 void hidapiw_native::enumerate(hid_device_info*& devs, unsigned short vendorID, unsigned short productID)
 {
-	_devs = hid_enumerate(vendorID, productID);
-	devs = _devs;
+	devs = hid_enumerate(vendorID, productID);
+}
+
+void hidapiw_native::freeEnumerate(hid_device_info*& devs)
+{
+	hid_free_enumeration(devs);
 }
 
 void hidapiw_native::open(int& devIdx, unsigned short vendorID, unsigned short productID, const wchar_t* serialNumber)
@@ -35,7 +35,104 @@ void hidapiw_native::open(int& devIdx, unsigned short vendorID, unsigned short p
 	{
 		throw hid_error(_dev);
 	}
-	std::lock_guard<std::recursive_mutex> lk(devMap_mutex);
+	addDevice(_dev, devIdx);
+}
+
+void hidapiw_native::openByPath(int& devIdx, const char* path)
+{
+	auto _dev = hid_open_path(path);
+	if (_dev == nullptr)
+	{
+		throw hid_error(_dev);
+	}
+	addDevice(_dev, devIdx);
+}
+
+void hidapiw_native::close(int devIdx)
+{
+	hid_device* _dev;
+	if (findDeviceInMap(devIdx, _dev))
+	{
+		hid_close(_dev);
+		devMap.erase(devIdx);
+	}
+}
+
+int hidapiw_native::findDeviceInMap(int devIdx, hid_device*& _dev)
+{
+	std::lock_guard<std::recursive_mutex> guard(devMap_mutex);
+	auto itr = devMap.find(devIdx);
+	if (itr != devMap.end())
+	{
+		_dev = itr->second;
+		return 1;
+	}
+	return 0;
+}
+
+int hidapiw_native::write(int devIdx, const unsigned char* data, size_t length)
+{
+	hid_device* _dev;
+	if (findDeviceInMap(devIdx, _dev))
+	{
+		return hid_write(_dev, data, length);
+	}
+	return 1;
+}
+
+int hidapiw_native::read_timeout(int devIdx, unsigned char*& data, size_t length, int milliseconds)
+{
+	hid_device* _dev;
+	if (findDeviceInMap(devIdx, _dev))
+	{
+		return hid_read_timeout(_dev, data, length, milliseconds);
+	}
+	return 1;
+}
+
+int hidapiw_native::read(int devIdx, unsigned char*& data, size_t length)
+{
+	hid_device* _dev;
+	if (findDeviceInMap(devIdx, _dev))
+	{
+		return hid_read(_dev, data, length);
+	}
+	return 1;
+}
+
+int hidapiw_native::set_nonblocking(int devIdx, int nonblock)
+{
+	hid_device* _dev;
+	if (findDeviceInMap(devIdx, _dev))
+	{
+		return hid_set_nonblocking(_dev, nonblock);
+	}
+	return 1;
+}
+
+int hidapiw_native::send_feature_report(int devIdx, const unsigned char* data, size_t length)
+{
+	hid_device* _dev;
+	if (findDeviceInMap(devIdx, _dev))
+	{
+		return hid_send_feature_report(_dev, data, length);
+	}
+	return 1;
+}
+
+int hidapiw_native::get_feature_report(int devIdx, unsigned char*& data, size_t length)
+{
+	hid_device* _dev;
+	if (findDeviceInMap(devIdx, _dev))
+	{
+		return hid_get_feature_report(_dev, data, length);
+	}
+	return 1;
+}
+
+void hidapiw_native::addDevice(hid_device* _dev, int& devIdx)
+{
+	std::lock_guard<std::recursive_mutex> guard(devMap_mutex);
 	devIdx = devMap.size();
-	devMap.insert(std::pair<hid_device*, int>(_dev, devIdx));
+	devMap.insert(std::pair<int, hid_device*>(devIdx, _dev));
 }
--- hidapiw_native/hidapiw_native.h
+++ hidapiw_native/hidapiw_native.h
@@ -8,15 +8,28 @@
 class hidapiw_native
 {
 private:
-	std::map<hid_device*, int> devMap;
+	std::map<int, hid_device*> devMap;
 #ifdef NATIVE_CODE
 	std::recursive_mutex devMap_mutex;
 #endif
-	struct hid_device_info* _devs;
+
+	void addDevice(hid_device* _dev, int& devIdx);
+	int findDeviceInMap(int devIdx, hid_device*& _dev);
 public:
 	hidapiw_native();
 	~hidapiw_native();
 	void enumerate(struct hid_device_info*& devs, unsigned short vendorID, unsigned short productID);
+	void freeEnumerate(hid_device_info*& devs);
 	void open(int& devIdx, unsigned short vendorID, unsigned short productID, const wchar_t* serialNumber = nullptr);
+	void openByPath(int& devIdx, const char* path);
+	void close(int devIdx);
+
+
+	int  write(int devIdx, const unsigned char* data, size_t length);
+	int read_timeout(int devIdx, unsigned char*& data, size_t length, int milliseconds);
+	int  read(int devIdx, unsigned char*& data, size_t length);
+	int  set_nonblocking(int devIdx, int nonblock);
+	int send_feature_report(int devIdx, const unsigned char* data, size_t length);
+	int get_feature_report(int devIdx, unsigned char*& data, size_t length);
 };
 
